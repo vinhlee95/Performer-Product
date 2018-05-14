5285e0e33deb5a3ea629a9f03ec11e71387c38b2
Vinhlee95 <lethanhvinh95@gmail.com>
2018-05-14 14:38:37 +0300

    Fix some styles for mobile view

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ProductCard from '../UI/ProductCard/ProductCard';
 import classes from './Product.css';
 import ProductImage from '../../images/product-image.jpg';
+import ProfileIcon from '../../images/profile-icon.svg';
 
 const Product = (props) => {
    return(
       <ProductCard>
          <div className={classes.imageContainer} >
             <img src={ProductImage} alt="product-img" className={classes.productImage} />
-            <span className={classes.price} >Price</span>
+            <div className={classes.priceRow}>
+               <span className={classes.price} >Price</span>
+               <div>
+                  <img src={ProfileIcon} alt="profile-icon" className={classes.profileIcon} />
+                  <span>Hassunhauska</span>
+               </div>
+            </div>
          </div>
          Here is product information
       </ProductCard>
