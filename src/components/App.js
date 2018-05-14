ca911b1222f6b42ff0dcb5f5687c38a7f81a53c5
Vinhlee95 <lethanhvinh95@gmail.com>
2018-05-14 13:24:14 +0300

    Change button style & render on screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import Button from './Button/Button';
+import Button from './UI/Button/Button';
+import classes from './App.css';
 
 class App extends Component {
    render() {
       return(
-         <div>
+         <div className={classes.App} >
             <h2>Performers & Products</h2>
-            <Button name="+ Lisää esiintyjä" />
-            <Button name="+ Lisää esitys" />
+            <div className={classes.buttonRow} >
+               <Button name="+ Lisää esiintyjä" />
+               <Button name="+ Lisää esitys" />
+            </div>
          </div>
       );
    }
