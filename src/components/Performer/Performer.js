db5332c8f1655bc3985549e98f6a8cf2736ba8e7
Vinhlee95 <lethanhvinh95@gmail.com>
2018-05-14 13:44:50 +0300

    Add reusable name card

--- src/components/Performer/Performer.js
+++ src/components/Performer/Performer.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import classes from './Performer.css';
-import Card from '../UI/Card/Card';
+import NameCard from '../UI/NameCard/NameCard';
 import ProfileIcon from '../../images/profile-icon.svg';
 
 class Performer extends Component {
    render() {
       return(
          <div className={classes.performerContainer} >
-            <Card>
+            <NameCard>
                <img src={ProfileIcon} alt="performer-profile-icon" className={classes.profileIcon} />
                <span>Hassunhauska</span>
-            </Card>
+            </NameCard>
          </div>
       );
    }
--- src/components/UI/NameCard/NameCard.js
+++ src/components/UI/NameCard/NameCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './Card.css';
+import classes from './NameCard.css';
 
 const Card = (props) => {
    return(
